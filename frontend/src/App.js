91d8ae3193007f9b9549726e37971be9f51aa01a
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2024-02-10 11:09:00 -0500

    conditionally rendering either sign up page or nutrition page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,39 @@
 
 //const apiKey = "AYk2KQqKxqck8qURepMzOgwYMoUFy2W2ClW2KP2P";
 
-//import React, { useState, useEffect } from 'react';
+//React Hook Imports
+import React, { useState } from 'react';
+
+//Component Imports
 import NutritionInfo from './Components/NutritionInfo';
 
+//Page Imports
+import SignInOrUpPage from './pages/SignInOrUpPage';
+
+//Context Imports
+import { UserContext } from "./context/UserContext"
+
 function App() {
+  const [user, setUser] = useState("");
 
   return(
+    <UserContext.Provider value={{user, setUser}}>
+
     <div className="App">
-        <h1 className="App-title">Welcome to React</h1>
+      {
+        user? (
+          <>
+        <h1 className="App-title"> Calorie Adherence Tracking App</h1>
         <NutritionInfo></NutritionInfo>
+          </>
+          ) : (
+            <SignInOrUpPage/>
+          )
+      }
+
     </div>
+
+    </UserContext.Provider>
   )
 }
 
