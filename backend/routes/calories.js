8d490511ec7f8abb385d74d33bf24fd6a5b8f2d9
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2024-02-11 20:14:13 -0500

    routes for calories added

--- backend/routes/calories.js
+++ backend/routes/calories.js
@@ -9,7 +9,7 @@ const router = new Router();
  * @description retrieves all daily calorie consumption entries for a user
  */
 
-router.get('/api/calories', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
       const calories = await CalorieConsumption.find({ user: req.user._id });
       res.json(calories);
@@ -24,15 +24,53 @@ router.get('/api/calories', async (req, res) => {
  * POST /
  * @description to add a new daily calorie consumption entry
  */
+router.post('/', async (req, res) => {
+  try {
+    const newCalorie = new CalorieConsumption({
+      user: req.user._id,
+      calories: req.body.calories,
+      date: req.body.date
+    });
+    const calories = await newCalorie.save();
+    res.json(calories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
 
 /**
  * PUT /
  * @description to update a specific daily calorie consumption entry
  */
 
+router.put('/:id', async (req, res) => {
+  try {
+    const calories = await CalorieConsumption.findByIdAndUpdate(
+      req.body.id,
+      req.body,
+      { new: true }
+    );
+    res.json(calories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
 /**
  * DELETE /
  * @description to delete a specific daily calorie consumption entry by its ID
  */
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const calories = await CalorieConsumption.findByIdAndDelete(req.body.id);
+    res.json(calories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
 
 export default router;
