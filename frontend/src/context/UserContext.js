8cf9e8571f8a6c1106578afbdbd0aef37d42a55b
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2024-02-14 13:08:55 -0500

    user context exported into and used in App.js

--- frontend/src/context/UserContext.js
+++ frontend/src/context/UserContext.js
@@ -1,30 +1,9 @@
-import React, { createContext, useState } from "react";
-import SignInOrUpPage from '../pages/SignInOrUpPage.js';
-import Calendar from "../Components/Calendar.js"
+import { createContext } from "react";
+// import SignInOrUpPage from '../pages/SignInOrUpPage.js';
+// import Calendar from "../Components/Calendar.js"
 
 
 // Create UserContext
-const UserContext = createContext(null);
+export const UserContext = createContext(null);
 
-// Define UserProvider component
-function UserProvider() {
-  // Initialize user state
-  const [user, setUser] = useState({});
 
-  return (
-    <UserContext.Provider value={{ user, setUser }}>
-      <div className="App">
-        {user ? (
-          <>
-            <h1 className="App-title">Calorie Adherence Tracking App</h1>
-            <Calendar> </Calendar>
-          </>
-        ) : (
-          <SignInOrUpPage />
-        )}
-      </div>
-    </UserContext.Provider>
-  );
-}
-
-export default UserProvider;
